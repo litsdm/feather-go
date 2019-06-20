cf39c80030d68999fc74446bb8abbf7f77dc03f9
cdiezmoran <cdiezmoran@gmail.com>
2019-06-20 11:31:37 -0500

    Fix mongodb query

--- features/file.go
+++ features/file.go
@@ -1,13 +1,64 @@
-package feather
+package file
 
-import "time"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"feather.com/internal"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type File struct {
-	Name      string    `json:"name" bson:"name"`
-	Size      float64   `json:"size" bson:"size"`
-	S3Url     string    `json:"s3Url" bson:"s3Url"`
-	Type      string    `json:"type" bson:"type"`
-	IsGroup   bool      `json:"isGroup" bson:"isGroup"`
-	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
-	ExpiresAt time.Time `json:"expiresAt" bson:"expiresAt"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Name      string             `json:"name" bson:"name"`
+	Size      float64            `json:"size" bson:"size"`
+	S3Url     string             `json:"s3Url" bson:"s3Url"`
+	Type      string             `json:"type" bson:"type"`
+	IsGroup   bool               `json:"isGroup" bson:"isGroup"`
+	CreatedAt time.Time          `json:"createdAt" bson:"createdAt"`
+	ExpiresAt time.Time          `json:"expiresAt" bson:"expiresAt"`
+}
+
+func Routes(configuration *config.Config) *chi.Mux {
+	router := chi.NewRouter()
+	// router.Post("/", CreateFile(configuration))
+	// router.Delete("/{userId}/files/{fileId}", DeleteFile(configuration))
+	// router.Get("/", GetAllFiles(configuration))
+	router.Get("/{userId}", GetUserFiles(configuration)) // MOVE TO USER
+	return router
+}
+
+func GetUserFiles(configuration *config.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userID := chi.URLParam(r, "userId")
+		objectID, _ := primitive.ObjectIDFromHex(userID)
+		filter := bson.D{{"to", objectID}}
+
+		collection := configuration.Database.Collection("files")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		cursor, err := collection.Find(ctx, filter)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer cursor.Close(ctx)
+
+		var results []*File
+		for cursor.Next(ctx) {
+			file := &File{}
+			er := cursor.Decode(file)
+			if er != nil {
+				log.Fatal(er)
+			}
+			results = append(results, file)
+		}
+
+		render.JSON(w, r, results)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module feather.com
 go 1.12
 
 require (
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-chi/render v1.0.1
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/golangci/golangci-lint v1.17.1
 	github.com/mongodb/mongo-go-driver v1.0.3 // indirect
 	github.com/spf13/viper v1.4.0
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
